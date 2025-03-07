43bca2a3f5e8d150ae3176a866243d7cb18aea68
dev-akash-sarker <akashsarker210@gmail.com>
2025-03-07 19:46:47 +0600

    little

--- src/app/components/hotsales/Hotsales.tsx
+++ src/app/components/hotsales/Hotsales.tsx
@@ -1,4 +1,3 @@
-import { h1 } from "framer-motion/client";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -37,8 +36,8 @@ const Hotsales: FC = () => {
   return (
     <>
       <div className="w-[1250px] mx-auto ">
-        <div className="flex">
-          <div className="bg-[rgba(22,31,51,70)] rounded-[20px] -m-10 z-40">
+        <div className="flex gap-10">
+          <div className="bg-[rgba(22,31,51,70)] rounded-[20px] -mt-10 z-40">
             <h3 className=" flex items-center gap-3 text-white font-bold text-xl mb-3 mt-6 ml-6">
               <Image
                 src="/hotsale/hot.png"
@@ -48,6 +47,52 @@ const Hotsales: FC = () => {
               />
               <span className=" inline"> Hot Sale</span>
             </h3>
+
+            <div className="flex hotsalehover w-[322px] mb-6 mx-6 flex-wrap  gap-3">
+              {hotsales.map((item, i) => (
+                <>
+                  <div
+                    key={i}
+                    className="w-[155px] h-[60px] bg-gradient-to-l from-purple-500 via-indigo-200 to-purple-100 relative rounded-[10px]"
+                  >
+                    <div className="w-[147px] h-[52.9px] bg-[#161F33] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 outline-[4.5px] outline-[#334155] hover:outline-transparent rounded-md flex items-center gap-3 p-2">
+                      <Image
+                        src={item.image}
+                        width={44}
+                        height={44}
+                        alt={item.title}
+                        className=" rounded-[10px]"
+                      />
+                      <h6 className=" text-[#D6E0EB] text-base font-normal">
+                        {item.title}
+                      </h6>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="bg-[rgba(22,31,51,70)] rounded-[20px] -mt-10 z-40">
+            <div className="m-6 w-[389px h-[56px] border border-[#334155] rounded-[10px]">
+              <select
+                className="z-20 w-full h-full bg-[#0F172A] text-white p-[16px] border-r-[16px] border-transparent outline-none rounded-[10px]"
+                name="cars"
+                id="cars"
+              >
+                <option value="volvo">
+                  <Image
+                    src="/hotsale/skull.png"
+                    width={24}
+                    height={24}
+                    alt="skull"
+                  />
+                  Volvo
+                </option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+              </select>
+            </div>
 
             <div className="flex hotsalehover w-[322px] mb-6 mx-6 flex-wrap  gap-3">
               {hotsales.map((item, i) => (
